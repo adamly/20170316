c58eacde0cf5fccdc3e618b21bdbf6d5c4439769
adamlyz <adamly@cht.com.tw>
2017-03-15 21:58:39 +0800

    2

--- src/test.java
+++ src/test.java
@@ -3,6 +3,7 @@ public class test {
     public static void main(String[] args) {
     	
     	System.out.println("hello world in PC");;
-    
+    	System.out.println("hello world in NB");;
+
     }
 }
